bde6873e320492148fc620a5046e1082c89fe41a
rubanero14 <85555894+rubanero14@users.noreply.github.com>
2022-12-09 11:19:16 +0800

    Added SQL Script file

---
+++ people.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE csharp_lesson_one;
+
+USE csharp_lesson_one;
+
+CREATE TABLE people (
+	Id INT NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+INSERT INTO 
+	people(
+	 FirstName,
+	 LastName
+	)
+VALUES 
+	(
+		'Wonder',
+		'Woman'
+	),
+	(
+		'Cat',
+		'Woman'
+	),
+	(
+		'Captain',
+		'Planet'
+	),
+	(
+		'Mickey',
+		'Mouse'
+	),
+	(
+		'Minnie',
+		'Mouse'
+	),
+	(
+		'The',
+		'Hulk'
+	);
+
+SELECT * FROM people;
+
+/* DROP TABLE csharp_lesson_one.people; */
+
+
+
+
